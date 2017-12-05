96ba204cd7462336096b74d378adcf05369a9dd5
Obongo Dokwub <obongo.dokwub@icloud.com>
2017-12-05 14:46:45 +0100

    Created FromBodyString attribute

---
+++ WebApi.Kit/ModelBinding/Attributes/FromBodyStringAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using WebApi.Kit.ModelBinding.Binders;
+
+namespace WebApi.Kit.ModelBinding.Attributes
+{
+    public class FromBodyStringAttribute : BinderTypeProviderAttribute
+    {
+        public override Type BinderType
+        {
+            get { return typeof(BodyStringModelBinder); }
+        }
+    }
+}
---
+++ WebApi.Kit/ModelBinding/Binders/BodyStringModelBinder.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Kit.ModelBinding.Binders
+{
+    public class BodyStringModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext.HttpContext.Request.ContentLength > 0)
+            {
+                if (bindingContext.ModelType == typeof(string))
+                {
+                    string value = bodyToString(bindingContext.HttpContext.Request.Body);
+
+                    bindingContext.Result = ModelBindingResult.Success(value);
+                }
+            }
+
+            return Task.FromResult(0);
+        }
+
+        private string bodyToString(Stream bodyStream)
+        {
+            using (var reader = new StreamReader(bodyStream, Encoding.UTF8))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
